2a3e82b8f462a576cb3ba4cb0cc1050fe1027ba5
Strand McCutchen <strand@nird.us>
2013-06-09 09:21:00 -0700

    Default Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitgrapher::Application.config.secret_token = '3610b8b3e49b40c30ec5b72336f3923b26f61937dce9cac1a568b1a6890eb9afbb34850be332ac57f7d99edf0accd6326dd962e8267445ca240d973b0dc376f0'
